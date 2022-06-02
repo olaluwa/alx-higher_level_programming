884134a33135267255584b5304aa1770ac13f3e4
olaluwa <oluwapelumiola3@gmail.com>
2022-06-02 13:01:47 +0100

    my first tool box

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ from calculator_1 import add, sub, mul, div
 def main():
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".function(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".function(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".function(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".function(a, b, div(a, b)))
+    print("{} + {} = {}".function(a, b, add(a, b)))
+    print("{} - {} = {}".function(a, b, sub(a, b)))
+    print("{} * {} = {}".function(a, b, mul(a, b)))
+    print("{} / {} = {}".function(a, b, div(a, b)))
 
-i __name__ == "__main__":
+if __name__ == "__main__":
     main()
