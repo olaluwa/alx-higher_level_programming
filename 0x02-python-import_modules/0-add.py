f356d6c1fb165d9fd885c1b8533bf243455f9712
olaluwa <oluwapelumiola3@gmail.com>
2022-06-02 12:33:56 +0100

    0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ = "__main__":
+    """
+
+    print the result of addition betwen 2 numbers
+
+    """
+
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
