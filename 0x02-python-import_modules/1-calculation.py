9ebe5dffc26187f11ad502023269489c16ece5ca
olaluwa <oluwapelumiola3@gmail.com>
2022-06-02 12:53:50 +0100

    my first tool box

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from calculatoer_1 import add, sub, mul, div
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".function(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".function(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".function(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".function(a, b, div(a, b)))
